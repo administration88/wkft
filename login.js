4718419de8c65baca01184729719b539eb852e74
administration88 <62919716+administration88@users.noreply.github.com>
2020-04-05 15:14:49 +0900

    newmake3

---
+++ login.js
@@ -0,0 +1,24 @@
+function fieldChanged(){
+	var userId = getField("user_id");
+	var password = getField("password");
+	var disabled = true;
+
+	if (userId.value.length > 0 && password.value.length > 0) {
+		disabled = false;
+	}
+
+	var login = getField("login");
+	if (disabled) {
+		login.setAttribute("disabled", "disabled");
+	}
+	else {
+		login.removeAttribute("disabled");
+	}
+}
+function getField(fieldName){
+	var field = document.getElementById(fieldName);
+	if (field == undefined) {
+		throw new Error("要�?が見つかりません: " + fieldName);
+	}
+	return field;
+}
